90edcb252380eed57fdcfa4174ab5d0e9104b00e
Ethan Marsh <ethandmarsh@gmail.com>
2023-12-17 12:00:40 -0500

    Started making objects for day5

--- day5/internal/almanac/almanac.go
+++ day5/internal/almanac/almanac.go
@@ -1,8 +1,28 @@
 package almanac
 
+import (
+	"regexp"
+)
+
+type Map struct {
+	destinationRangeStart int
+	sourceRangeStart      int
+	rangeLength           int
+}
+
 type Almanac struct {
+	seeds                  []int
+	seedToSoilMaps         []Map
+	soilToFertilizers      []Map
+	fertilizerToWaters     []Map
+	waterToLights          []Map
+	lightToTemperatures    []Map
+	temperatureToHumiditys []Map
+	humidityToLocations    []Map
 }
 
 func NewAlmanac(lines []string) Almanac {
+	seedsRegex, _ = regexp.Compile("")
+
 	return Almanac{}
 }
